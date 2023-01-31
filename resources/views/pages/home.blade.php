e6b05d96efb2f925f72916e2eb4d4d7e47890e2c
Jac22 <jacopo.prudentino@gmail.com>
2023-01-31 18:23:12 +0100

    Stampate tutte le informazioni dei film

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,9 +2,26 @@
 
 @section('content')
     <h1>Movies</h1>
-    <ul>
-        @foreach ($movies as $movie)
-            <li>{{$movie -> title}}</li>
-        @endforeach
-    </ul>
+    <table class="table table-primary">
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Original title</th>
+                <th>Nationality</th>
+                <th>Date</th>
+                <th>Vote</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($movies as $movie)
+                <tr>
+                    <td>{{$movie -> title}}</td>
+                    <td>{{$movie -> original_title}}</td>
+                    <td>{{$movie -> nationality}}</td>
+                    <td>{{$movie -> date}}</td>
+                    <td>{{$movie -> vote}}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
